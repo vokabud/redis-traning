3fc8d0a48eda80f9a203efe134415dae468271a1
vokabud <60111632+vokabud@users.noreply.github.com>
2021-11-27 18:49:48 +0200

    Initial commin
    Add initial redis client
    Set date time to redis (key dt)

---
+++ RedisPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace RedisPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appSettings.json"))
+                .Build();
+
+            var host = config["host"];
+            var port = config["port"];
+
+            Console.WriteLine($"Host: {host}");
+            Console.WriteLine($"Port: {port}");
+
+            var redisClient = new RedisClient(host, port);
+
+            Console.WriteLine($"Set dt variable with current date");
+
+            redisClient
+                .Set("dt", DateTime.Now.ToString("r"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RedisPractice/RedisClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisPractice
+{
+    internal class RedisClient
+    {
+        private readonly string Host;
+        private readonly string Port;
+
+        public RedisClient(
+            string host,
+            string port)
+        {
+            Host = host;
+            Port = port;
+        }
+
+        public void Set(string key, string value)
+            => Connect()
+                .GetDatabase()
+                .StringSet(
+                    key: key,
+                    value: new RedisValue(value));
+
+        public string Get(string key)
+        {
+            using var redis = Connect();
+            var database = redis.GetDatabase();
+
+            if (!database.KeyExists(key))
+            {
+                throw new ArgumentException();
+            }
+
+            var value = database
+                .StringGet(key: key);
+
+            if (value.HasValue)
+            {
+                return value;
+
+            }
+
+            return default;
+        }
+
+        private ConnectionMultiplexer Connect()
+            => ConnectionMultiplexer
+                .Connect(
+                    new ConfigurationOptions
+                    {
+                        EndPoints = {$"{Host}:{Port}"}
+                    });
+    }
+}
